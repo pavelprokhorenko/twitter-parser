3067aa1704b40487c94e51a3081144e67d38307e
pavelprokhorenko <prohorenkopavel74@gmail.com>
2022-12-24 10:26:37 +0400

    setup settings

---
+++ settings.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+from pydantic import BaseSettings
+from yaml import SafeLoader, load
+
+CONFIG_FILE = f"{Path(__file__).parent.absolute()}/settings.yaml"
+
+
+class Service(BaseSettings):
+    """
+    Settings for service.
+    """
+
+    host: str
+    port: int
+    workers: int
+    debug: bool
+    origins: list[str]
+
+
+class Twitter(BaseSettings):
+    """
+    Settings for Twitter API.
+    """
+
+    api_key: str
+    api_secret_key: str
+    api_bearer_token: str
+    api_access_token: str
+    api_access_token_secret: str
+
+
+class Settings(BaseSettings):
+    service: Service
+    twitter: Twitter
+
+
+def setup_settings() -> Settings:
+    """
+    Setup settings from config file.
+    """
+    with open(CONFIG_FILE) as f:
+        config = load(f, SafeLoader)
+        return Settings.parse_obj(config)
+
+
+settings = setup_settings()
