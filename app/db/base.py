9366463c6f882d244eee4c1d31b47dd87aa446b7
pavelprokhorenko <prohorenkopavel74@gmail.com>
2022-12-24 13:34:09 +0400

    setup sqlalchemy

---
+++ app/db/base.py
@@ -0,0 +1,28 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from settings import settings
+
+postgres = settings.postgres
+
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://{}:{}@{}:{}/{}".format(
+    postgres.user,
+    postgres.password,
+    postgres.host,
+    postgres.port,
+    postgres.database,
+)
+
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    echo=settings.service.env.upper() == "DEV",
+    pool_pre_ping=True,
+)
+
+async_session = sessionmaker(
+    engine,
+    class_=AsyncSession,
+    autoflush=False,
+    autocommit=False,
+    expire_on_commit=False,
+)
